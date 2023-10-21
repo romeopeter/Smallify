a12c691707516cac9e6f272b89f03d4fe339388f
romeo <romeomeshack34@gmail.com>
2023-10-22 00:37:07 +0100

    Optimised main process to to fit Electron-vite tooling

--- src/main.js
+++ src/main.js
@@ -1,24 +1,34 @@
 const path = require("path");
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow} = require("electron");
 
 const isMac = process.platform === "darwin";
 const isDev = process.env.NODE_ENV === "development";
 
-function createMainWindow() {
-  const win = new BrowserWindow({ title: "Smallify", with: 800, height: 600 });
+// Main window
+function mainWindow() {
+  const win = new BrowserWindow({
+    title: "Smallify",
+    with: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "../preload/script.js"),
+    },
+  });
 
   // Open dev tools if in development mode
   if (isDev) win.webContents.openDevTools();
 
-  win.loadFile(path.join(__dirname, "renderer/index.html"));
+  win.loadURL("http://localhost:5173/")
 }
 
+// App launches and is ready
 app.whenReady().then(() => {
-  createMainWindow();
+  mainWindow();
+
 
   // Open window if none is open (MacOS)
   app.on("activate", () => {
-    if (BrowserWindow.getAllWindows().length === 0) createMainWindow();
+    if (BrowserWindow.getAllWindows().length === 0) mainWindow();
   });
 });
 
