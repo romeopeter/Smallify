e2fea0a4fba1cc3441c9151552b90f5dcb163045
romeo <romeomeshack34@gmail.com>
2023-10-27 12:38:21 +0100

    Implemented menu

--- src/main.js
+++ src/main.js
@@ -1,8 +1,39 @@
 const path = require("path");
-const { app, BrowserWindow} = require("electron");
+const { app, BrowserWindow, Menu } = require("electron");
 
 const isMac = process.platform === "darwin";
 const isDev = process.env.NODE_ENV === "development";
+
+const menuTemplate = [
+  { role: "appMenu" },
+  ...(isMac
+    ? [
+        {
+          label: app.name,
+          submenu: [
+            { role: "about", click: () => aboutWindow() },
+            { type: "separator" },
+            { role: "services" },
+            { type: "separator" },
+            { role: "hide" },
+            { role: "hideOthers" },
+            { role: "unhide" },
+            { type: "separator" },
+            { role: "quit" },
+          ],
+        },
+      ]
+    : []),
+  { role: "fileMenu" },
+  ...(!isMac
+    ? [
+        {
+          label: "Help",
+          submenu: [{ label: "About", click: () => aboutWindow() }],
+        },
+      ]
+    : []),
+];
 
 // Main window
 function mainWindow() {
@@ -18,13 +49,30 @@ function mainWindow() {
   // Open dev tools if in development mode
   if (isDev) win.webContents.openDevTools();
 
-  win.loadURL("http://localhost:5173/")
+  win.loadURL("http://localhost:5173/");
+}
+
+// About window
+function aboutWindow() {
+  const win = new BrowserWindow({
+    title: "Smallify | About",
+    with: 300,
+    height: 300,
+    webPreferences: {
+      preload: path.join(__dirname, "../preload/script.js"),
+    },
+  });
+
+  win.loadURL("http://localhost:5173/");
 }
 
 // App launches and is ready
 app.whenReady().then(() => {
   mainWindow();
 
+  // App menu
+  const mainMenu = Menu.buildFromTemplate(menuTemplate);
+  Menu.setApplicationMenu(mainMenu);
 
   // Open window if none is open (MacOS)
   app.on("activate", () => {
