c0231bb35d70b82a00638822ca520cfa4d1a1824
romeo <romeomeshack34@gmail.com>
2023-10-31 23:08:28 +0100

    Modified

--- electron.vite.config.js
+++ electron.vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     build: {
       outDir: "dist/main",
       lib: {
-        entry: resolve("src/main.js"),
+        entry: resolve("src/main/index.js"),
       },
     },
   },
