a156f5ce6590b2d61dd671ce5b5d0a7cfec78c00
romeo <romeomeshack34@gmail.com>
2023-10-31 21:31:51 +0100

    Main process

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 const path = require("path");
-const { app, BrowserWindow, Menu } = require("electron");
+const os = require("os");
+const { readFileSync, existsSync, mkdirSync, writeFileSync } = require("fs");
+const { app, BrowserWindow, Menu, ipcMain, shell } = require("electron");
+const ResizeImg = require("resize-img");
 
 const isMac = process.platform === "darwin";
 const isDev = process.env.NODE_ENV === "development";
@@ -35,21 +38,30 @@ const menuTemplate = [
     : []),
 ];
 
+// Exposed main window in the global scope
+let mainWindow
+
 // Main window
-function mainWindow() {
-  const win = new BrowserWindow({
+function createMainWindow() {
+  mainWindow = new BrowserWindow({
     title: "Smallify",
     with: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, "../preload/script.js"),
+      preload: path.join(__dirname, "../preload/preloadScript.js"),
+      sandbox: false,
     },
   });
 
-  // Open dev tools if in development mode
-  if (isDev) win.webContents.openDevTools();
+  // Open dev tools when in development mode
+  if (isDev) mainWindow.webContents.openDevTools();
 
-  win.loadURL("http://localhost:5173/");
+  // Load the remote URL for development or the local html file for production.
+  if (isDev && process.env["ELECTRON_RENDERER_URL"]) {
+    mainWindow.loadURL(process.env["ELECTRON_RENDERER_URL"]);
+  } else {
+    mainWindow.loadFile(path.join(__dirname, "../renderer/index.html"));
+  }
 }
 
 // About window
@@ -59,26 +71,69 @@ function aboutWindow() {
     with: 300,
     height: 300,
     webPreferences: {
-      preload: path.join(__dirname, "../preload/script.js"),
+      contextIsolation: true,
+      nodeIntegration: true,
+      preload: path.join(__dirname, "./preload/preloadScript.js"),
     },
   });
 
-  win.loadURL("http://localhost:5173/");
+  // Load the remote URL for development or the local html file for production.
+  if (isDev && process.env["ELECTRON_RENDERER_URL"]) {
+    win.loadURL(process.env["ELECTRON_RENDERER_URL"]);
+  } else {
+    win.loadFile(path.join(__dirname, "../renderer/index.html"));
+  }
 }
 
 // App launches and is ready
 app.whenReady().then(() => {
-  mainWindow();
+  createMainWindow();
 
   // App menu
   const mainMenu = Menu.buildFromTemplate(menuTemplate);
   Menu.setApplicationMenu(mainMenu);
+
+  // Response to IPC renderer message on channel "image:resize"
+  ipcMain.on("image:resize", (_event, data) => {
+    data.dest = path.join(os.homedir(), "smallify");
+    resizeImage(data);
+  });
+
+  // Remove main window from memory when closed
+  mainWindow.on("close", () => mainWindow = null);
 
   // Open window if none is open (MacOS)
   app.on("activate", () => {
     if (BrowserWindow.getAllWindows().length === 0) mainWindow();
   });
 });
+
+// Resize image
+async function resizeImage({ width, height, imgPath, dest }) {
+  try {
+    const newPath = await ResizeImg(readFileSync(imgPath), {
+      width: +width,
+      height: +height,
+    });
+
+    // File name
+    const filename = path.basename(imgPath);
+
+    // Create file destination if not available
+    if (!existsSync(dest)) mkdirSync(dest);
+
+    // Write new file to destination
+    writeFileSync(path.join(dest, filename), newPath)
+
+    // Send success message
+    mainWindow.webContents.send("image:done")
+
+    // Open folder to newly resize image
+    shell.openPath(dest);
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 app.on("window-all-closed", () => {
   if (!isMac) app.quit();
